187fd3722582d2325ba8db4316987a13559c7173
Volcanify <noemail@noemail.com>
2025-10-23 16:59:48 +0600

    Updates

--- _app/immutable/chunks/BHEyHRxQ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_181mo35?.base??"/civical",a=globalThis.__sveltekit_181mo35?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DIQg0tK2.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_o67iyj?.base??"/civical",a=globalThis.__sveltekit_o67iyj?.assets??s??"";export{a,s as b};
