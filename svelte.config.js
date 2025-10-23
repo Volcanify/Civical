63bb8f3d0c78e56ed0237bc1dfcbf574194455d7
Volcanify <noemail@noemail.com>
2025-10-23 19:10:15 +0600

    reference: sveltekit-gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,19 +8,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		alias: {
       "@/*": "./path/to/lib/*",
     },
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/civical' : '',
-			relative: false
 		}
 	}
 };
