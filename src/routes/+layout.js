04657d22273b747f6d917427e084ab726917c718
Volcanify <noemail@noemail.com>
2025-10-24 12:25:47 +0600

    added posthog analystics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,14 @@
 export const prerender = true;
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+  if (browser) {
+    posthog.init('phc_WqSMXohypdxpBdGEFrJBIcTwzn0f1yKauzKY6UbxJHg', {
+      api_host: 'https://us.i.posthog.com',
+      defaults: '2025-05-24',
+    })
+  }
+
+  return
+};
