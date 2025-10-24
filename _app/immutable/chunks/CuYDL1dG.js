32364ceabc3bc6ef587f52046bd61b1ae30510af
Volcanify <noemail@noemail.com>
2025-10-24 16:37:03 +0600

    Updates

--- _app/immutable/chunks/CuYDL1dG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ys33gx?.base??"/civical",a=globalThis.__sveltekit_1ys33gx?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.Ci-N6HdL.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CP1v9Doi.js";export{o as load_css,r as start};
